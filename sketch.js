46424d3619d135bbf8ac213b0e8b5989f5945eee
unknown <pradnyakhare212@gmail.com>
2020-12-20 19:32:15 +0530

    updated sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 function setup() {
-  createCanvas(800,400);
+
+  createCanvas(480,800);
   createSprite(400, 200, 50, 50);
+
 }
 
 function draw() {
-  background(255,255,255);  
+
+  background(0,0,0);  
   drawSprites();
+
 }
